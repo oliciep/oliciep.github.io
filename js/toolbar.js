85536d08fdca464107953d8adbccb91f48b9b6fe
Oliver Cieplinski <olivercieplinski@gmail.com>
2024-01-26 23:38:23 +0000

    Minor code pruning and code organisation

--- js/toolbar.js
+++ js/toolbar.js
@@ -1,8 +1,8 @@
 const sections = document.querySelectorAll("section");
 
 window.addEventListener("scroll", () => {
-  let currentSectionId = "home"; // Default to the first section
-  const offset = window.innerHeight * 0.4; // Adjust as needed
+  let currentSectionId = "home"; 
+  const offset = window.innerHeight * 0.4;
 
   sections.forEach((section) => {
     if (window.scrollY >= section.offsetTop - offset) {
@@ -10,7 +10,6 @@ window.addEventListener("scroll", () => {
     }
   });
 
-  // Add the "active" class to the corresponding link in the toolbar
   document.querySelectorAll(".toolbar a").forEach((link) => {
     link.classList.remove("active");
     if (link.getAttribute("href") === `#${currentSectionId}`) {
